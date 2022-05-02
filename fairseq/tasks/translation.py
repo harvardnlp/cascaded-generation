f14bca039a356347c92e628672b1e396476fbd0e
George Kamtziridis <georgekam96@gmail.com>
2022-05-03 00:42:32 +0300

    Resolved `sacrebleu` compatibility issue.
    In 2.* version of the `sacrebleu` module, the `compute_bleu` method is now part of `sacrebleu.metrics.BLEU`, instead of `sacrebleu`. We've resolved that issue by allowing both 1.* and 2.* version to work properly.

--- fairseq/tasks/translation.py
+++ fairseq/tasks/translation.py
@@ -313,13 +313,22 @@ class TranslationTask(FairseqTask):
 
                 def compute_bleu(meters):
                     import inspect
-                    import sacrebleu
-                    fn_sig = inspect.getfullargspec(sacrebleu.compute_bleu)[0]
+                    try:
+                        from sacrebleu.metrics import BLEU
+
+                        comp_bleu = BLEU.compute_bleu
+                    except ImportError:
+                        # Otherwise, we fall back to API version 1.*
+                        import sacrebleu
+
+                        comp_bleu = sacrebleu.compute_bleu
+
+                    fn_sig = inspect.getfullargspec(comp_bleu)[0]
                     if 'smooth_method' in fn_sig:
                         smooth = {'smooth_method': 'exp'}
                     else:
                         smooth = {'smooth': 'exp'}
-                    bleu = sacrebleu.compute_bleu(
+                    bleu = comp_bleu(
                         correct=meters['_bleu_counts'].sum,
                         total=meters['_bleu_totals'].sum,
                         sys_len=meters['_bleu_sys_len'].sum,
